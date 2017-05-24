c1c31a5b75c2b5c39ab7f6a19ddfe6c50b0c0f0c
Chaz Henricks <chazhenricks@gmail.com>
2017-05-24 11:25:22 -0500

    getting movie info from database

--- js/db-interactions.js
+++ js/db-interactions.js
@@ -15,8 +15,20 @@ function getMovie(movie) {
 	});
 }
 
+function getActors(movieID) {
+	return new Promise(function(resolve, reject){
+		$.ajax({
+			url: `https://api.themoviedb.org/3/movie/${movieID}/credits?api_key=7cf213fd59bc986d0eb48bf0aead461a`
+		}).done(function(movieData){
+			resolve(movieData);
+		}).fail(function(error){
+			reject(error);
+		});
+	});
+}
 
 
 module.exports = {
-	getMovie
+	getMovie,
+	getActors
 };
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ console.log("MAIN.JS");
 
 let $ = require('jquery'),
     db = require("./db-interactions"),
+		Handlebars=require("hbsfy/runtime"),
+		cardsTemplate = require("../templates/cards.hbs"),
     // templates = require("./dom-builder"),
     user = require("./user");
 
@@ -18,12 +20,26 @@ $("#auth-btn").click(function(){
 });
 
 $("#find-new-movies").click(function(){
+	var newMovieObj = {};
 	console.log("find new movies clicked");
 	var inputItem = $("#input").val();
 	console.log("inputItem", inputItem);
 	db.getMovie(inputItem)
 	.then(function(movieData){
-	console.log("movie data", movieData);
+		console.log(movieData);
+		newMovieObj = movieData.results[0];
+	
+	db.getActors(newMovieObj.id)
+	.then(function(actors){
+		newMovieObj.cast = [];
+		for (var i=0; i<5;i++){
+			newMovieObj.cast.push(actors.cast[i]);
+		}
+		console.log("movie data", newMovieObj);
+		console.log("movie data", newMovieObj.cast[0]);
+	});
+	$(".movies").html(cardsTemplate(newMovieObj));
+
 	});
 });
 
